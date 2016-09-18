2fef1a177880a945ced56fa228b4b007c3ba16e5
Michiel <michiel.vanwijk@timbuktutravel.com>
2016-09-18 15:46:59 +0200

    Meh, work in progress, taking a break

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,19 +8,29 @@
     /*
       Simple mobile navigation
     */
+    // Mobile
     var select = document.getElementById('mobile-nav');
-
     select.onchange = function() {
-
       let hash = this.getElementsByTagName('option')[this.selectedIndex].value;
-
       $('body').animate({
         scrollTop: $('#' + hash).position().top
       }, 300, function() {
         window.location.hash = hash;
       });
-
     };
+
+    //Desktop
+    $('.nav.desktop a').click(function(e) {
+      e.preventDefault();
+      let hash = this.hash;
+      $('body').animate({
+        scrollTop: $(hash).position().top
+      }, 300, function() {
+        window.location.hash = hash;
+      });
+    });
+
+
 
   });
 
