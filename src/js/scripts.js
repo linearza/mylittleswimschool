996b76038500499c07ad290fa679fe571bd1ad0c
Michiel <michiel.vanwijk@timbuktutravel.com>
2016-10-02 14:59:26 +0200

    Adds real content. Adds table styling. Adds ajax contact form. Makes styling changes as required.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,7 +4,6 @@
 
   $(function() {
 
-
     /*
       Simple mobile navigation
     */
@@ -19,7 +18,7 @@
       });
     };
 
-    //Desktop
+    // Desktop
     $('.nav.desktop a').click(function(e) {
       e.preventDefault();
       let hash = this.hash;
@@ -27,6 +26,36 @@
         scrollTop: $(hash).position().top
       }, 300, function() {
         window.location.hash = hash;
+      });
+    });
+
+    // Contact form
+    var $contactForm = $('#contact-form'),
+      $submitButton = $('#contact-form input[type=submit]');
+    $contactForm.submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '//formspree.io/vanwijk.mc@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $submitButton.prop('disabled', true);
+          $submitButton.before('<div class="alert sending">Sending message…</div>');
+        },
+        success: function() {
+          $contactForm.find('.alert.sending').remove();
+          $submitButton.before('<div class="alert sent">Message sent!</div>');
+          $submitButton.prop('disabled', false);
+          setTimeout(function() {
+            $contactForm.find('.alert.sent').remove();
+          }, 5000);
+        },
+        error: function(err) {
+          $contactForm.find('.alert.sending').remove();
+          $submitButton.before('<div class="alert error">Oops, there was an error:' + err + '. Please contact me directly at daniella@mylittleswimschool.co.za</div>');
+          $submitButton.prop('disabled', false);
+        }
       });
     });
 
